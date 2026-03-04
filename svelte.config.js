5b4b95fe83ba39550f4801226a8feeaa306b0838
Shahzaib Imtiaz <145100452+shahzaib1233@users.noreply.github.com>
2026-03-04 11:09:46 +0500

    all udpated owrk

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,16 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
-	}
+    appDir: 'internal', // This replaces '_app' with 'internal'
+    adapter: adapter()
+  }
 };
 
 export default config;
